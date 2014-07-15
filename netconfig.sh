5c20aceadc3f9861b1cb8289b94570f05a0a3613
tortuser <tortuser@debian>
2014-07-15 02:13:20 +0200

    Now working start.sh script.

--- netconfig.sh
+++ netconfig.sh
@@ -14,13 +14,15 @@ SUBNET=$3
 SITE_SERVER_PORT=$4
 CALIBRE_SERVER_PORT=$5
 
+echo $CALIBRE_SERVER_PORT
 
-sudo service NetworkManager stop
+sudo service network-manager stop
+sudo service apache2 stop
 sudo service dnsmasq start
 
 sudo iptables -t nat -F #Cleaning iptables
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -i $ETH0 -j DNAT --to $ADDR:$SITE_SERVER_PORT
 sudo iptables -t nat -A PREROUTING -p tcp --dport $CALIBRE_SERVER_PORT -i $ETH0 -j DNAT --to $ADDR:$CALIBRE_SERVER_PORT
 
-calibre-server --daemonize $CALIBRE_SERVER_PORT
-python2 ./server.py $SITE_SERVER_PORT
+calibre-server -p $CALIBRE_SERVER_PORT &
+python2 ./server.py $SITE_SERVER_PORT &
